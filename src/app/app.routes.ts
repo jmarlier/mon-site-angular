6292bcd9134031145020f5b80a58ea2b1f0ad73d
jmarlier <jeromemarlier@gmail.com>
2025-09-03 09:17:17 +0200

    démarrage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Portfolio } from './pages/portfolio/portfolio';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Accueil - Mon Site' },
+  { path: 'about', component: About, title: 'À propos - Mon Site' },
+  { path: 'portfolio', component: Portfolio, title: 'Portfolio - Mon Site' },
+  { path: 'contact', component: Contact, title: 'Contact - Mon Site' },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirection pour les routes inconnues
+];
